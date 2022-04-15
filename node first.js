008316bfc4fd38315357a9fbd56fcc3dfd7356bf
Olivia jingyao <oliviachen797@gmail.com>
2022-04-15 12:59:34 +1000

    first

---
+++ node first.js
@@ -0,0 +1,14 @@
+var first = 'hello nodejs';
+console.log(first);
+
+function fn() {
+    console.log('fn function');
+}
+fn();
+
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+if (true) {
+    console.log('123');
+}
