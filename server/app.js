5c3749cc72e316b413d6975ec658e470a6d35110
Olivia jingyao <oliviachen797@gmail.com>
2022-04-18 22:20:44 +1000

    http

--- server/app.js
+++ server/app.js
@@ -4,7 +4,15 @@ const http = require('http');
 const app = http.createServer();
 //为网站服务器添加请求事件,有请求来触发事件，添加事件 处理函数
 app.on('request', (req, res) => {
-        res.end('<h2>HELLO USER</h2>')
+        //获取请求方式
+        // req.method
+        console.log(req.method);
+        if (req.method == 'POST') {
+            res.end('post')
+        } else if (req.method == 'GET') {
+            res.end('get')
+        }
+        // res.end('<h2>HELLO USER</h2>')
     })
     //监听端口才可以
 app.listen(3000)
