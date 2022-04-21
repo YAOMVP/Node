af8da4935f997f47b86c1b519c1a0f489c5cb4e1
Olivia jingyao <oliviachen797@gmail.com>
2022-04-21 23:50:58 +1000

    http

--- static/public/app.js
+++ static/public/app.js
@@ -1,12 +1,12 @@
-//创建网站服务器的模块
+//1.创建网站服务器的模块
 const http = require('http');
-//app对象就是网站服务器对象
+//2.app对象就是网站服务器对象
 const app = http.createServer();
-//为网站服务器添加请求事件,有请求来触发事件，添加事件 处理函数
+//3.为网站服务器添加请求事件,有请求来触发事件，添加事件 处理函数
 app.on('request', (req, res) => {
     res.end('okhi');
 })
 
-//监听端口才可以
+//4.监听端口才可以
 app.listen(3000)
 console.log('OK');
