5918acf440f94f924ad5768fea329fde1ca0a88f
Olivia jingyao <oliviachen797@gmail.com>
2022-05-13 21:42:08 +1000

    app.use

---
+++ framework/11.js
@@ -0,0 +1,30 @@
+//😊讲的是app.use：
+
+const express = require('express')
+const bodyParse = require('body-parser')
+
+const app = express()
+
+app.use(fn({ a: 1 }))
+
+function fn(obj) {
+    return function(req, res, next) {
+        if (obj.a == 1) {
+            console.log(req.url);
+        } else {
+            console.log(req.method);
+        }
+        next()
+    }
+}
+
+
+app.post('/add', (req, res) => {
+    //接收post请求参数
+    res.send(req.body)
+})
+
+
+//监听端口
+app.listen(3000)
+console.log('网站服务器启动成功了');
