2d2a41cd6172dee244f0985b24730632e764b001
Olivia jingyao <oliviachen797@gmail.com>
2022-06-16 12:02:13 +1000

    done

---
+++ code/get name/get name .js
@@ -0,0 +1,14 @@
+// $(function() {
+//     let heroName = $(".hero").val().trim();
+//     if (text.html === "") {
+//         return "";
+//     }
+//     $(".search").on("click", function() {
+//         $.get({
+//             url: "https://autumnfish.cn/api/hero/simple?name = heroName",
+//             success: function(backData) {
+//                 console.log(backData);
+//             }
+//         })
+//     })
+// })
