4b32b4709a63cbf4e32f81494ae6d8e053e2e501
Olivia jingyao <oliviachen797@gmail.com>
2022-08-18 15:06:52 +1000

    half done

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { log } = require("node:console");
 const https = require("node:https");
 const app = express();
 const port = 3000;
@@ -7,10 +8,35 @@ app.get("/", (req, res) => {
     const url = "https://api.weatherapi.com/v1/current.json?key=f5122e86b67d46a1b12132731221708&q=Hobart&aqi=yes";
 
     https.get(url, (response) => {
-        console.log(response);
+        console.log(response.statusCode);
+        console.log("headers:", response.headers);
+
+        response.on("data", (data) => {
+            process.stdout.write(data);
+            console.log(data);
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+
+            const location = weatherData.location.tz_id;
+
+            const temp = weatherData.current.temp_c;
+
+            const text = weatherData.current.condition.text;
+
+            const iconCode = weatherData.current.condition.icon;
+
+            console.log(`The location is: ${location} ,and the temperature is: ${temp} degree Celcius.`);
+            console.log(iconCode);
+
+            res.write(`<h1>The location is: ${location} </h1>`);
+            res.write(`<h2>The temperature is: ${temp} degree Celcius.</h2>`);
+            res.write(`<h3>The weather is currently: ${text} outside.</h3>`);
+            res.write(`<img src = ${iconCode}>`);
+            res.send();
+
+        });
     })
 
-    res.send("The server is up and running!");
 });
 
 
